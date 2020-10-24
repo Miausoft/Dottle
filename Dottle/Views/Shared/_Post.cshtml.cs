c835655daf9336cd034cfc531abf43dcc6216991
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2020-10-24 23:32:14 +0300

    Extract Post into a partial view, render posts using HtmlRenderer in index

---
+++ Dottle/Views/Shared/_Post.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dottle.Views.Shared
+{
+    public class _Post : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
