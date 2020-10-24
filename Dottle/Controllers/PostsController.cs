90ae53132fef5a353ef9e4208e7237176e7bc2c4
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2020-10-24 22:22:48 +0300

    Add basic Post validations

--- Dottle/Controllers/PostsController.cs
+++ Dottle/Controllers/PostsController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Dottle.Models;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Dottle.Controllers
 {
@@ -36,9 +37,41 @@ namespace Dottle.Controllers
         public async Task<JsonResult> Create(string jsonPost)
         {
             PostModel post = JsonConvert.DeserializeObject<PostModel>(jsonPost);
-            await db.Posts.AddAsync(post);
-            await db.SaveChangesAsync();
-            return Json("Saved!");
+            List<string> errors = ValidatePost(post);
+            if (errors.Count == 0)
+            {
+                await db.Posts.AddAsync(post);
+                await db.SaveChangesAsync();
+                return Json("Your post has been successfully created!");
+            }
+
+            return Json(string.Join("\n", errors));
+        }
+
+        private List<string> ValidatePost(PostModel post)
+        {
+            List<string> errors = new List<string>();
+            if (string.IsNullOrEmpty(post.Title))
+            {
+                errors.Add("Error: Title can't be empty!");
+            }
+
+            if (string.IsNullOrEmpty(post.PhoneNumber))
+            {
+                errors.Add("Error: Phone number can't be empty!");
+            }
+            
+            if (string.IsNullOrEmpty(post.Email))
+            {
+                errors.Add("Error: Email can't be empty!");
+            }
+            
+            if (string.IsNullOrEmpty(post.Address))
+            {
+                errors.Add("Error: Address can't be empty!");
+            }
+
+            return errors;
         }
         
     }
