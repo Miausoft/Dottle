b0c1d9cbb7ef81d74ffbf681e7857ef69b484891
Nenartavičiūtė Laura <laura.nenartaviciute@mif.stud.vu.lt>
2020-11-18 23:50:35 +0200

    Finish implementing timeSheet editing

---
+++ Dottle/Models/DayParam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dottle.Models
+{
+    public class DayParam
+    {
+        public string Name;
+        public bool Marked;
+    }
+}
