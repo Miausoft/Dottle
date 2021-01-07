e8e1573f3a708ec581a0b4b5b030ae786fa925ff
Nenartavičiūtė Laura <laura.nenartaviciute@mif.stud.vu.lt>
2021-01-07 04:05:11 +0200

    Fix bug

--- Dottle/wwwroot/js/site.js
+++ Dottle/wwwroot/js/site.js
@@ -111,21 +111,5 @@ $(document).ready(() => {
         toggleCarts(e.target)
     });
 
-    function saveVotes(e) {
-        const el = $(e.target);
-        const parent = el.parent();
-        const postId = parent.find('.post-id').prop('value');
-        $.ajax({
-            url: '/Posts/RatePost/' + postId,
-            data: { rating: el.data('rating') },
-            type: 'POST',
-            success: function (result) {
-                if (parent.attr('value') === "false") {
-                    parent.find('.CurrentScore').html(result);
-                    parent.attr('value', 'true');
-                }
-                console.log("Result is:", result);
-            }
-        });
-    }
+    
 });
