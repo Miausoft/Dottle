cfa32c187b5a5cb6d1fbe1198fdf5bd5e37531ee
saltanovas <zumbass@gmail.com>
2021-05-23 17:14:39 +0300

    Rename

---
+++ Dottle.Web/Models/ServiceDbContext.cs
@@ -0,0 +1,18 @@
+﻿/*using Microsoft.EntityFrameworkCore;
+
+namespace Dottle.Models
+{
+    public class ServiceDbContext : DbContext
+    {
+
+        public ServiceDbContext(DbContextOptions<ServiceDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        
+        public DbSet<PostModel> Posts { get; set; }
+    }
+}
+*/
