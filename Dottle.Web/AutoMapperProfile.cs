2ef7cf02af6232dbbffd936b25b8d5a9c02144bb
saltanovas <zumbass@gmail.com>
2021-05-24 00:05:50 +0300

    Automapper

---
+++ Dottle.Web/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Dottle.Domain.Entities;
+using Dottle.Web.Models.User;
+
+namespace Dottle.Web
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<CreateViewModel, User>();
+        }
+    }
+}
