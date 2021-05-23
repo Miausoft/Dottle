d2f326a80203add9ea2e641d7451797520143849
saltanovas <zumbass@gmail.com>
2021-05-23 22:57:26 +0300

    Generic repository

---
+++ Dottle.Persistence/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Dottle.Persistence.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        public IQueryable<T> GetAll();
+        public Task<T> GetByIdAsync(params object[] keyValues);
+        public IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate);
+        public ValueTask<EntityEntry<T>> InsertAsync(T entity);
+        public void Delete(T entity);
+        public Task SaveAsync();
+    }
+}
---
+++ Dottle.Persistence/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Dottle.Persistence.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DatabaseContext _context;
+        private readonly DbSet<T> _table;
+
+        public Repository(DatabaseContext context)
+        {
+            _context = context;
+            _table = _context.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _table;
+        }
+
+        public async Task<T> GetByIdAsync(params object[] keyValues)
+        {
+            return await _table.FindAsync(keyValues);
+        }
+
+        public IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate)
+        {
+            return _table.Where(predicate);
+        }
+
+        public ValueTask<EntityEntry<T>> InsertAsync(T entity)
+        {
+            return _table.AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _table.Remove(entity);
+        }
+
+        public Task SaveAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+    }
+}
