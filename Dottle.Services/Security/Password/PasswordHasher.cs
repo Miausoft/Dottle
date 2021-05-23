f656d34f46683e3726aa333319f31b2c66f83675
saltanovas <zumbass@gmail.com>
2021-05-23 22:56:48 +0300

    Password hash service

---
+++ Dottle.Services/Security/Password/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿namespace Dottle.Services.Security.Password
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        private const int Cost = 11;
+
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, workFactor: Cost);
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
