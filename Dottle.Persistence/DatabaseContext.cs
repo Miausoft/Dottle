bdc397ac7af673b9b18e639826e422bca8a82fae
saltanovas <zumbass@gmail.com>
2021-05-23 17:13:15 +0300

    Database context

---
+++ Dottle.Persistence/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Dottle.Domain.Entities;
+using System.Reflection;
+
+namespace Dottle.Persistence
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+        public DbSet<TimeSheet> TimeSheets { get; set; }
+        public DbSet<Rate> Rates { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
