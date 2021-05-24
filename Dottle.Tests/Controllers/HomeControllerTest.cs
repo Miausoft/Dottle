ee6299fbab8ff7046d0e18d29eb4ff1662cbd7d3
povilasleka <povilasleka@gmail.com>
2021-05-24 21:37:15 +0300

    HomeControllerTest.cs tests created.

--- Dottle.Tests/Controllers/HomeControllerTest.cs
+++ Dottle.Tests/Controllers/HomeControllerTest.cs
@@ -5,6 +5,7 @@ using Dottle.Domain.Entities;
 using Dottle.Persistence;
 using Dottle.Persistence.Repository;
 using Dottle.Web.Controllers;
+using Dottle.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -22,17 +23,55 @@ namespace Dottle.Tests.Controllers
         {
         }
 
+        private HomeController MockHomeController(DatabaseContext context)
+        {
+            var repository = new Repository<Post>(context);
+            var confMock = new Mock<IConfiguration>();
+            return new HomeController(repository, confMock.Object);
+        }
+
+        
+
         [Fact]
         public void Index_ShouldReturnViewResultWithPostList()
         {
             using var context = new DatabaseContext(ContextOptions);
-            var repository = new Repository<Post>(context);
-            var confMock = new Mock<IConfiguration>();
-            var homeController = new HomeController(repository, confMock.Object);
+            var homeController = MockHomeController(context);
 
             var result = homeController.Index().GetAwaiter().GetResult() as ViewResult;
 
+            Assert.NotNull(result);
+            Assert.NotNull(result.Model);
+            Assert.IsType<List<Post>>(result.Model);
+        }
 
+        [Fact]
+        public void Settings_ShouldReturnViewResultNamedSettings()
+        {
+            using var context = new DatabaseContext(ContextOptions);
+            var homeController = MockHomeController(context);
+
+            var result = homeController.Settings() as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.Model);
+            Assert.IsType<UserSetting>(result.Model);
+        }
+
+        [Fact]
+        public void UpdateSettings_ShouldReturnRedirectResult_WithValidUserSettingsData()
+        {
+            using var context = new DatabaseContext(ContextOptions);
+            var homeController = MockHomeController(context);
+            UserSetting userSetting = new UserSetting
+            {
+                SiteLayout = string.Empty,
+            };
+
+            var result = homeController.UpdateSettings(userSetting) as RedirectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal("/", result.Url);
         }
     }
 }
