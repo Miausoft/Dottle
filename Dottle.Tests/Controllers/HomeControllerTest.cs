7a35c5b4fe4e77a758fc56b8e518d92199ca0963
povilasleka <povilasleka@gmail.com>
2021-05-24 21:06:41 +0300

    BaseControllerTest.cs and HomeControllerTest.cs

---
+++ Dottle.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dottle.Domain.Entities;
+using Dottle.Persistence;
+using Dottle.Persistence.Repository;
+using Dottle.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Xunit;
+
+namespace Dottle.Tests.Controllers
+{
+    public class HomeControllerTest : BaseControllerTest
+    {
+        public HomeControllerTest() : base(
+            new DbContextOptionsBuilder<DatabaseContext>()
+            .UseInMemoryDatabase("DottleDB")
+            .Options) 
+        {
+        }
+
+        [Fact]
+        public void Index_ShouldReturnViewResultWithPostList()
+        {
+            using var context = new DatabaseContext(ContextOptions);
+            var repository = new Repository<Post>(context);
+            var confMock = new Mock<IConfiguration>();
+            var homeController = new HomeController(repository, confMock.Object);
+
+            var result = homeController.Index().GetAwaiter().GetResult() as ViewResult;
+
+
+        }
+    }
+}
