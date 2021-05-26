f68297a1e6d4629320a6166147d7a0fcc0d67862
saltanovas <zumbass@gmail.com>
2021-05-26 20:36:00 +0300

    Enum with week days

---
+++ Dottle.Domain/Enums/Days.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dottle.Domain.Enums
+{
+    public enum Days
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
